e89bfa96011f1ac07bbd65a2a816b01bda2d8a14
oyamasiphula <oyama@projectcodex.co>
2016-04-08 16:33:29 +0200

    still need to do the workouts

---
+++ kata.js
@@ -0,0 +1,11 @@
+var peoplesBigDataSummary =  [];
+
+function person(name, sayings) {
+    this.username = name;
+    this.userQuote = sayings;
+};
+
+var Earner1 = new person("Walter","“It was 67MB. Is that everything? I’m busy, you know.”")
+var Earner2 = new person("Jesse","“Damn, it was like 300MB. Dat Snaptalk is a data hog, yo.”")
+var Earner3 = new person("Saul","“I think 283MB. That’s what the guy at the store said. I think it means MegaBites.”")
+var Earner4 = new person("Gus","“Around 150MB. I have the receipts if you need more precise figures.”")
