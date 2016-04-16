15e2482e27da1cefa6043b73403587d042ae7e6a
oyamasiphula <oyama@projectcodex.co>
2016-04-16 15:36:54 +0200

    new way of javascripting objects map

---
+++ userDataManipulation.js
@@ -0,0 +1,26 @@
+var userDetails = [
+  {
+    username:"Walter",
+    userQuote: "“It was 67MB. Is that everything? I’m busy, you know.”"
+  },
+  {
+    username: "Jesse",
+    userQuote: "“Damn, it was like 300MB. Dat Snaptalk is a data hog, yo.”"
+  },
+  {
+    username: "Saul",
+    userQuote: "“I think 283MB. That’s what the guy at the store said. I think it means MegaBites.”"
+  },
+  {
+    username:"Gus",
+    userQuote: "“Around 150MB. I have the receipts if you need more precise figures.”"
+  }
+];
+
+
+// var userDatacheck = function(userDetails){
+    userDetails.forEach(function(userProperties){
+      console.log(userProperties);;
+    })
+
+userDatacheck();
